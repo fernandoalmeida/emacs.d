57faa30cf6c020ef2a4ac668d989923fa53fb9c4
Fernando Almeida <fernando@fernandoalmeida.net>
2025-04-21 03:02:46 -0300

    [elpaca] setup elpaca

---
+++ README.md
@@ -0,0 +1,10 @@
+# A simple and modular Emacs config by @fernandoalmeida
+
+This Emacs config adds improved defaults and extended support for the following:
+
+* Elixir
+* Python
+* Javascript / Typescript
+* HTML / HEEX / Markdown
+* CSS / LESS / SASS / SCSS
+* YAML / TOML / XML
---
+++ init.el
@@ -0,0 +1,18 @@
+;;; init.el --- Startup entry point
+
+;;; Commentary:
+
+;; This is the entry point of the setup.
+;; The config is splitted into multiple files.
+
+;;; Code:
+
+;; Produce backtraces when errors occur: can be helpful to diagnose startup issues
+;;(setq debug-on-error t)
+
+(add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
+
+(require 'setup-elpaca)
+
+(provide 'init)
+;;; init.el ends here
