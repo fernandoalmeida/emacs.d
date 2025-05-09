d62b2b8e6199a78992dbe5a648d96ad45ddcac62
Fernando Almeida <fernando@fernandoalmeida.net>
2025-05-09 10:49:13 -0300

    [emacs] enable auto removing trailing whitespace on save

--- config/setup-emacs.el
+++ config/setup-emacs.el
@@ -8,13 +8,13 @@
 
 (use-package emacs
   :ensure nil
-  :config 
+  :config
   (delete-selection-mode 1)
   (fset 'yes-or-no-p 'y-or-n-p)
   (global-hl-line-mode t)
   (global-display-line-numbers-mode)
   (menu-bar-mode -1)
-  (setq 
+  (setq
    auto-save-default nil
    column-number-mode t
    create-lockfiles nil
@@ -31,12 +31,14 @@
   (tool-bar-mode -1)
   (electric-indent-mode -1)
   (electric-pair-mode 1)
+  (cua-mode t)
+  (setq cua-enable-cua-keys nil)
+  :hook
+  (before-save . delete-trailing-whitespace)
   :custom
   (enable-recursive-minibuffers t)
   (read-extended-command-predicate #'command-completion-default-include-p)
   (minibuffer-prompt-properties '(read-only t cursor-intangible t face minibuffer-prompt))
-  (cua-mode t)
-  (setq cua-enable-cua-keys nil)
   :bind
   ("M-p" . (lambda () (interactive) (previous-line 5)))
   ("M-n" . (lambda () (interactive) (next-line 5)))
