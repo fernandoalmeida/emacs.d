8703375e0b45221fd1a43f0bbdb14282ed02d4f1
Fernando Almeida <fernando@fernandoalmeida.net>
2025-05-09 13:03:44 -0300

    [git] setup git-timemachine

--- config/setup-git.el
+++ config/setup-git.el
@@ -3,7 +3,7 @@
 ;;; Commentary:
 
 ;; * Magit
-;; 
+;;
 ;; Magit is an interface to the version control system Git, implemented as an
 ;; Emacs package. It is complete enough to allow users to perform almost all
 ;; of their daily version control tasks directly from within Emacs.
@@ -22,7 +22,11 @@
 (use-package magit-todos
   :after magit
   :ensure t
-  :config (magit-todos-mode))
+)
+
+(use-package git-timemachine
+  :ensure t
+)
 
 (provide 'setup-git)
 ;; setup-git.el ends here
