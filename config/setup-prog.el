304f94ba7005c975999bd155bd6cba7bf7d87f2c
Fernando Almeida <fernando@fernandoalmeida.net>
2025-07-04 01:04:36 -0300

    [prog] remove unused setup

--- config/setup-prog.el
+++ config/setup-prog.el
@@ -34,18 +34,9 @@
   :ensure t
   :after treesit-auto
   :mode "\\.exs?\\'"
-  :init
-  (setq major-mode-remap-alist
-	'((elixir-mode . elixir-ts-mode)))
-  :config
-  (dolist (lang-source
-           '((elixir "https://github.com/elixir-lang/tree-sitter-elixir")
-             (heex   "https://github.com/phoenixframework/tree-sitter-heex")))
-    (add-to-list 'treesit-language-source-alist lang-source))
-
-  (dolist (lang '(elixir heex))
-    (unless (treesit-language-available-p lang)
-      (treesit-install-language-grammar lang)))
+  :hook ((elixir-ts-mode . eglot-ensure)
+	 (before-save . eglot-format))
+)
   :hook
   ((elixir-ts-mode . eglot-ensure))
 )
