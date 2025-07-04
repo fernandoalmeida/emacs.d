cbad427962cb7276c53c3e768d2659217ed97d2f
Fernando Almeida <fernando@fernandoalmeida.net>
2025-07-03 23:23:35 -0300

    [nav] customize grep to use active region as initial value

--- config/setup-nav.el
+++ config/setup-nav.el
@@ -21,9 +21,10 @@
   (vertico-mode 1)
   (vertico-buffer-mode 1)
   :config
-  (setq vertico-buffer-display-action '(display-buffer-in-side-window
-					(side . bottom)
-					(window-height . 0.3)))
+  (setq vertico-buffer-display-action
+	'(display-buffer-in-side-window
+	  (side . bottom)
+	  (window-height . 0.3)))
   )
 
 (use-package savehist
@@ -37,33 +38,45 @@
 
 (use-package consult
   :ensure t
+
   :hook
   (completion-list-mode . consult-preview-at-point-mode)
+
   :init
   (advice-add #'register-preview :override #'consult-register-window)
   (setq register-preview-delay 0.5)
   (setq xref-show-xrefs-function #'consult-xref xref-show-definitions-function #'consult-xref)
+
   :config
   (defun custom/consult-line ()
-    "Run `consult-line` using the active region as initial input, or blank."
+    "Run `consult-line` using the active region as initial input."
     (interactive)
     (let ((initial (when (use-region-p)
                      (buffer-substring-no-properties (region-beginning) (region-end)))))
       (consult-line initial)))
+  (defun custom/consult-ripgrep ()
+    "Grep selected region as initial input."
+    (interactive)
+    (let ((dir (or (consult--project-root) default-directory))
+	  (initial
+	   (when (use-region-p)
+             (buffer-substring-no-properties (region-beginning) (region-end)))))
+      (consult--grep "Ripgrep" #'consult--ripgrep-make-builder dir initial)))
   (consult-customize
    consult-theme :preview-key '(:debounce 0.2 any)
-   consult-ripgrep consult-git-grep consult-grep consult-man :initial (thing-at-point 'symbol)
+   consult-git-grep consult-grep consult-man
    consult-bookmark consult-recent-file consult-xref
    consult--source-bookmark consult--source-file-register
    consult--source-recent-file consult--source-project-recent-file
    :preview-key '(:debounce 0.4 any))
   (setq consult-narrow-key "<")
+
   :bind
   ("C-s" . custom/consult-line)
+  ("C-c g" . custom/consult-ripgrep)
   ("C-x b" . consult-buffer)
   ("M-y" . consult-yank-from-kill-ring)
-  ("C-c g" . consult-ripgrep)
-)
+  )
 
 (use-package embark
   :ensure t
