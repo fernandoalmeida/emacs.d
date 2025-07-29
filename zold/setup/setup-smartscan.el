aa5d7a4fdd90cc9293a8d5056c657f6f88ef7b9a
Fernando Almeida <fernando@fernandoalmeida.net>
2025-07-28 21:15:39 -0300

    [emacs] delete zold dir

--- zold/setup/setup-smartscan.el
+++
@@ -1,9 +0,0 @@
-;;; setup-smartscan --- Setup smartscan package
-;;; Commentary:
-;;; Code:
-(smartscan-mode 1)
-(global-set-key (kbd "C-.") 'smartscan-symbol-go-forward)
-(global-set-key (kbd "C-,") 'smartscan-symbol-go-backward)
-
-(provide 'setup-smartscan)
-;;; setup-smartscan ends here
